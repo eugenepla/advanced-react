41b21c89b1075bd6ece109ba4c68501d42068033
Eugene Plakhotnikov <plahotnikovvv@gmail.com>
2025-12-14 20:09:53 +0200

    css modules configuration

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import {Counter} from "./components/Counter";
+import './index.scss'
+
+const App = () => {
+  return (
+    <div className="app">
+      <Routes>
+        <Route path={'/about'} />
+        <Route path={'/'} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
