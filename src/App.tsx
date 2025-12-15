af9b7fb3d1c082e4061330694255907959d2387d
Eugene Plakhotnikov <plahotnikovvv@gmail.com>
2025-12-15 23:16:25 +0200

    added routing and used lazy components for chucks

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import {Route, Routes} from 'react-router-dom';
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
 import {Counter} from "./components/Counter";
 import './index.scss'
+import {AboutPageAsync} from "./pages/AboutPage/AbooutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 const App = () => {
   return (
     <div className="app">
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        <Route path={'/about'} />
-        <Route path={'/'} />
+          <Route path={'/about'} element={<AboutPageAsync />} />
+          <Route path={'/'} element={<MainPageAsync />} />
       </Routes>
+      </Suspense>
     </div>
   );
 };
